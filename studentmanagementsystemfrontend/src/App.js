ca30bc620e61547773c065900319174d3f477528
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2022-09-06 21:05:49 +0530

    Initialize project using Create React App

---
+++ studentmanagementsystemfrontend/src/App.js
@@ -0,0 +1,13 @@
+import Student from './components/Student';
+import StudentList from './components/StudentList';
+
+function App() {
+  return (
+    <div className="App">
+      <Student/>
+      <StudentList/>
+    </div>
+  );
+}
+
+export default App;
